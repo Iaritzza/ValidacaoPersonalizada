a064ab8cd0972da360907167208bc36601062e43
Iaritzza Bielinki <iaritzzayara@gmail.com>
2021-09-03 11:11:13 -0300

    Projeto criado

---
+++ ValidacaoPersonalizada/Models/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ValidacaoPersonalizada.Validação;
+
+namespace ValidacaoPersonalizada.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+        public string Nome { get; set; }
+        [AdultoAtribute]
+        public int Idade { get; set; }
+    }
+}
---
+++ ValidacaoPersonalizada/Validação/AdultoAtribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using ValidacaoPersonalizada.Models;
+
+namespace ValidacaoPersonalizada.Validação
+{
+    public class AdultoAtribute : ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            Pessoa pessoa = (Pessoa)validationContext.ObjectInstance;
+
+            if(pessoa.Idade < 18)
+            {
+                return new ValidationResult("Apenas adultos podem se cadastrar");
+            }
+            
+            return ValidationResult.Success;
+        }
+    }
+}
